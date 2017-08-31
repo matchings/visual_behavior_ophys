42b3dca445051518d48fc894ddacce4e6bba2e50
matchings <marinag@alleninstitute.org>
2017-08-30 22:22:09 -0700

    version 0.1

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+packages = find_packages()
+
+setup(name="visual_behavior__ophys",
+      version=0.1,
+      description="visual behavior ophys analysis",
+      author="marinag",
+      author_email="marinag@alleninstitute.org",
+      url="https://github.com/matchings/visual_behavior_ophys",
+      packages=packages,
+      requires=['pandas', 'numpy', 'scipy', 'PyDAQmx'],
+      include_package_data=True,
+      package_data={
+          "": ['*.png', '*.ico', '*.jpg', '*.jpeg'],
+          },
+      )
